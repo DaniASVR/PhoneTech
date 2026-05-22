568e74ada35ec1f537dcb09754b236fa857fa558
Daniel Varela <landsdani@gmail.com>
2026-05-22 18:37:55 +0200

    -configuracion de los archivos de docker-compose y dockerfile -creacion de index.php para verificar que el servicio php esta corriendo

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+// public/index.php
+
+// Mensaje de prueba para verificar que el servidor Apache y PHP funcionan
+echo "<h1>Entorno funcionando</h1>";
+echo "<p>PHP version: " . phpversion() . "</p>";
+echo "<p>index.php se esta ejecutando correctamente.</p>";

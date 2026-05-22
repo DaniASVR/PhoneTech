728331d1c7bab3693e672ded797407d6ad304267
Daniel Varela <landsdani@gmail.com>
2026-05-22 20:24:49 +0200

    -creacion del controlador home y prueba del home.php

---
+++ src/Views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PhoneTech - Inicio</title>
+</head>
+<body>
+    <h1>Esta es la pagina de home</h1>
+    <p>Bienvenido a PhoneTech, tu taller de reparacion de moviles de confianza.</p>
+</body>
+</html>
